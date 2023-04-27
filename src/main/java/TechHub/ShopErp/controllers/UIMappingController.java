b1add3d82a8982453c8d972a0f8b810a9a4a9e70
sachin karande <karandesd98@gmail.com>
2023-04-27 12:01:25 -0700

    hibernate added into project

--- src/main/java/TechHub/ShopErp/controllers/UIMappingController.java
+++ src/main/java/TechHub/ShopErp/controllers/UIMappingController.java
@@ -2,6 +2,7 @@ package TechHub.ShopErp.controllers;
 
 import java.security.Principal;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 import TechHub.ShopErp.Managers.UserManager;
 import TechHub.ShopErp.model.User;
+import TechHub.ShopErp.utilityAndSecurity.HibernateUtility;
 
 
 @Controller
@@ -23,6 +26,7 @@ public class UIMappingController {
 	@GetMapping("/")
 	public String home(Model m)
 	{
+	//	SessionFactory sessionFactory=HibernateUtility.getSessionFactory();  
 		m.addAttribute("title","home page");
 		return "home";
 	}
---
+++ src/main/java/TechHub/ShopErp/tables/User.java
@@ -0,0 +1,24 @@
+package TechHub.ShopErp.tables;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+}
