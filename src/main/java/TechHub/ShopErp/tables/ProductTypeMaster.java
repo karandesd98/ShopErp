d9c203b874c4ae3d16119549d539107b5fee1e84
sachin karande <karandesd98@gmail.com>
2023-05-03 18:56:51 -0700

    new table added

---
+++ src/main/java/TechHub/ShopErp/tables/ProductTypeMaster.java
@@ -0,0 +1,124 @@
+package TechHub.ShopErp.tables;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "PRODUCT_TYPE_MASTER")
+public class ProductTypeMaster {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="PRODUCT_TYPE_MASTER_ID")
+    private int  productTypeMasterId;
+	
+	@Column(name = "PRODUCT_TYPE_MASTER_NAME")
+	private String productTypeMasterName;
+	
+	@Column(name="UNIQUE_NO")
+    private int  unique_no;
+	
+	@Column(name="PRODUCT_TYPE_MASTER_PARENT_ID")
+    private int  productTypeMasterParentId;
+	
+	@Column(name="CREATED_BY")
+    private int  createdBy;
+	
+	@Column(name="DELETED_BY")
+    private int  deletedBy;
+	
+	@Column(name = "CREATED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedAt;
+	
+	@Column(name = "DELETED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+	@Column(name = "IS_DELETED", nullable = false, columnDefinition = "TINYINT(1)")
+	private boolean isDeleted;
+
+	public int getProductTypeMasterId() {
+		return productTypeMasterId;
+	}
+
+	public void setProductTypeMasterId(int productTypeMasterId) {
+		this.productTypeMasterId = productTypeMasterId;
+	}
+
+	public String getProductTypeMasterName() {
+		return productTypeMasterName;
+	}
+
+	public void setProductTypeMasterName(String productTypeMasterName) {
+		this.productTypeMasterName = productTypeMasterName;
+	}
+
+	public int getUnique_no() {
+		return unique_no;
+	}
+
+	public void setUnique_no(int unique_no) {
+		this.unique_no = unique_no;
+	}
+
+	public int getProductTypeMasterParentId() {
+		return productTypeMasterParentId;
+	}
+
+	public void setProductTypeMasterParentId(int productTypeMasterParentId) {
+		this.productTypeMasterParentId = productTypeMasterParentId;
+	}
+
+	public int getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public int getDeletedBy() {
+		return deletedBy;
+	}
+
+	public void setDeletedBy(int deletedBy) {
+		this.deletedBy = deletedBy;
+	}
+
+
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+
+}
---
+++ src/main/java/TechHub/ShopErp/tables/PurchaseOrder.java
@@ -0,0 +1,145 @@
+package TechHub.ShopErp.tables;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "PURCHASE_ORDER")
+public class PurchaseOrder {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="PURCHASE_ORDER_ID")
+    private int  purchaseOrderId;
+	
+	@Column(name = "PURCHASE_ORDER_NAME")
+	private String purchaseOrderName;
+	
+	@Column(name="PURCHASE_ORDER_TOTAL_AMOUNT")
+    private int  purchaseOrderTotalAmount;
+	
+	@Column(name = "PURCHASE_FROM")
+	private String purchaseFrom;
+	
+	@Column(name = "PURCHASE_FROM_MOBILE_NO")
+	private String purchaseFromMobileNo;
+	
+	@Column(name = "UPLOADED_BILL_PATH")
+	private String uploadedBillPath;
+	
+	@Column(name="CREATED_BY")
+    private int  createdBy;
+	
+	@Column(name="DELETED_BY")
+    private int  deletedBy;
+	
+	@Column(name = "CREATED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedAt;
+	
+	@Column(name = "DELETED_AT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+	
+	@Column(name = "IS_DELETED", nullable = false, columnDefinition = "TINYINT(1)")
+	private boolean isDeleted;
+
+	public int getPurchaseOrderId() {
+		return purchaseOrderId;
+	}
+
+	public void setPurchaseOrderId(int purchaseOrderId) {
+		this.purchaseOrderId = purchaseOrderId;
+	}
+
+	public String getPurchaseOrderName() {
+		return purchaseOrderName;
+	}
+
+	public void setPurchaseOrderName(String purchaseOrderName) {
+		this.purchaseOrderName = purchaseOrderName;
+	}
+
+	public int getPurchaseOrderTotalAmount() {
+		return purchaseOrderTotalAmount;
+	}
+
+	public void setPurchaseOrderTotalAmount(int purchaseOrderTotalAmount) {
+		this.purchaseOrderTotalAmount = purchaseOrderTotalAmount;
+	}
+
+	public String getPurchaseFrom() {
+		return purchaseFrom;
+	}
+
+	public void setPurchaseFrom(String purchaseFrom) {
+		this.purchaseFrom = purchaseFrom;
+	}
+
+	public String getPurchaseFromMobileNo() {
+		return purchaseFromMobileNo;
+	}
+
+	public void setPurchaseFromMobileNo(String purchaseFromMobileNo) {
+		this.purchaseFromMobileNo = purchaseFromMobileNo;
+	}
+
+	public String getUploadedBillPath() {
+		return uploadedBillPath;
+	}
+
+	public void setUploadedBillPath(String uploadedBillPath) {
+		this.uploadedBillPath = uploadedBillPath;
+	}
+
+	public int getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public int getDeletedBy() {
+		return deletedBy;
+	}
+
+	public void setDeletedBy(int deletedBy) {
+		this.deletedBy = deletedBy;
+	}
+
+	public Date getDeletedAt() {
+		return deletedAt;
+	}
+
+	public void setDeletedAt(Date deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public boolean isDeleted() {
+		return isDeleted;
+	}
+
+	public void setDeleted(boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	
+
+}
