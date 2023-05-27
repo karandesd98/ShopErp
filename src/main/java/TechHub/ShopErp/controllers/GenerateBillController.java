8adda3c024c659ad1b03b74261c9eb4f9078d0d5
sachin karande <karandesd98@gmail.com>
2023-05-27 06:36:13 -0700

    invoice releted changes

--- src/main/java/TechHub/ShopErp/controllers/GenerateBillController.java
+++ src/main/java/TechHub/ShopErp/controllers/GenerateBillController.java
@@ -1,5 +1,6 @@
 package TechHub.ShopErp.controllers;
 
+import java.io.FileOutputStream;
 import java.security.Principal;
 import java.util.List;
 
@@ -13,6 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.codec.Base64.OutputStream;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 import TechHub.ShopErp.Managers.InvoiceService;
 import TechHub.ShopErp.Managers.UserManager;
 import TechHub.ShopErp.exception.InvoiceNotFoundException;
@@ -20,8 +29,17 @@ import TechHub.ShopErp.model.User;
 import TechHub.ShopErp.pdf.InvoiceDataPdfExport;
 import TechHub.ShopErp.tables.Invoice;
 
+import com.twilio.twiml.voice.*;
+
 @Controller
 public class GenerateBillController {
+	
+	 // Twilio Account SID and Auth Token
+    private static final String TWILIO_ACCOUNT_SID = "ACe07dc864999575b70dfeb8d3130af1a9";
+    private static final String TWILIO_AUTH_TOKEN = "9a3c8678477bce7ce996a07d2c15ec2d";
+
+    // Twilio WhatsApp phone number
+    private static final String TWILIO_WHATSAPP_NUMBER = "+14155238886";
 
 	@Autowired
 	private InvoiceService service;
@@ -154,5 +172,63 @@ public class GenerateBillController {
 		// send to pdfImpl class
 		mav.addObject("list", list);
 		return mav;
+	}
+	
+	
+	@GetMapping("/shareInvoice")
+	public void shareinvoice() {
+		 try {
+	            // Generate the PDF
+	            Document document = new Document();
+	            FileOutputStream outputStream = new FileOutputStream("sample.pdf");
+	           // PdfWriter.getInstance(document, outputStream);
+
+	            document.open();
+	           // document.add(new Paragraph("Hello, World!"));
+	            document.close();
+	            outputStream.close();
+
+	            // Share the PDF via WhatsApp
+	            String pdfPath = "sample.pdf";
+	            String recipientNumber = "7378938893";
+
+	            sendPdfViaWhatsApp(pdfPath, recipientNumber);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	}
+		 
+		 public static void sendPdfViaWhatsApp(String pdfPath, String recipientNumber) {
+		        try {
+		            // Initialize Twilio client
+		            Twilio.init(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+		            // Read the PDF file
+		           // byte[] pdfBytes = readPdfFile(pdfPath);
+
+		            // Encode PDF file to Base64
+		           // String base64EncodedPDF = Base64.getEncoder().encodeToString(pdfBytes);
+
+		            // Send the PDF via WhatsApp
+		            Message.creator(
+		                    new PhoneNumber("whatsapp:+" + recipientNumber),
+		                    new PhoneNumber(TWILIO_WHATSAPP_NUMBER),
+		                    "").create();
+		                    //.setMediaUrl(new URL("data:application/pdf;base64," + base64EncodedPDF))
+		                   /* .create();*/
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		        
+		    
+
+				/*
+				 * public static byte[] readPdfFile(String filePath) throws Exception {
+				 * ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+				 * byte[] buffer = new byte[1024]; int bytesRead; try (InputStream inputStream =
+				 * new FileInputStream(filePath)) { while ((bytesRead =
+				 * inputStream.read(buffer)) != -1) { byteArrayOutputStream.write(buffer, 0,
+				 * bytesRead); } } return byteArrayOutputStream.toByteArray(); }
+				 */
 	}
 }
